d80475c9a18f76433a500de27eb700b8a17e331e
DOMENICALI leonardo <leonardo.domenicali@etu.u-paris.fr>
2024-01-24 16:06:37 +0100

    Distance matrix

--- test.py
+++ test.py
@@ -1,15 +1,58 @@
-import requests
 from geopy.geocoders import Nominatim
+import numpy as np
+import requests
+
 geolocator = Nominatim(user_agent="test")
 
-dep_city = geolocator.geocode("Bologna")
-arr_city = geolocator.geocode("Imola")
+class City:
+    def __init__(self,name,lon,lat):
+        self.name=name
+        self.lon=lon
+        self.lat=lat
+    
+    def __str__(self) :
+        return self.name + " " +self.coordinate()
+    
+    def coordinate (self) : 
+        return str(self.lon)+","+str(self.lat)
 
-url = 'http://router.project-osrm.org/route/v1/driving/'+str( dep_city.longitude) +','+ str(dep_city.latitude)+';'+str( arr_city.longitude) +','+ str(arr_city.latitude)
-r = requests.get(url)
-res = r.json()
-distance =res['routes'][0]['distance']
-duration = res['routes'][0]['duration']
+#popultate a list with name of the city and coordinate
+def populate (*string):
+    list = []
+    for str in string:
+        temp = geolocator.geocode(str,timeout=None)
+        city = City(str,temp.longitude,temp.latitude)
+        list.append(city)
+    
+    return list
 
-print("KM: "+str(distance/1000) )
-print("min: "+str(round(duration/60)))
+def get_distance(string):
+    #print(string)
+    url = 'http://router.project-osrm.org/route/v1/driving/'+string
+    r = requests.get(url)
+    res = r.json()
+    #print(res['routes'][0]['distance'])
+    distance = res['routes'][0]['distance']/1000
+    return round(distance,1)
+
+#fill matrix with distances between city, only compute for lower trinagular matrix but we can assume the distance not changing much and cut in half the time soo...
+def fill_matrix(list):
+    distance_matrix = np.zeros((len(list),len(list)))
+    for x in range(len(list)):
+        for y in range(x):
+            string = list[x].coordinate()+";"+list[y].coordinate()
+            distance_matrix[x][y] = get_distance(string)
+    
+    return np.tril(distance_matrix) + np.tril(distance_matrix, -1).T
+
+    
+list = populate("Bologna","Imola","Bari","Milano","Roma")
+
+distance_matrix= fill_matrix(list)
+
+#debug
+for city in list:
+    print(str(city))
+
+
+print(distance_matrix)
