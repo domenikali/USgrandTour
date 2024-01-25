755e4717e852833ca3aa87a2714edfe410047ba6
DOMENICALI leonardo <leonardo.domenicali@etu.u-paris.fr>
2024-01-25 14:45:16 +0100

    minimum spanning tree and map

--- README.md
+++ README.md
@@ -9,11 +9,16 @@ All distances will be in KM, and it will count the short distance by car.
 
 For the api I chose the OSMR API based on Opne Street Map, using the 'request' library in python is possible to recive the query and know the shortest driving distance between two points
 
-Using this API we can fill a matrix with the distance of the city between each other creating a connected graph 
+Using this API we can fill a matrix with the distance of the city between each other creating a complete graph
+
+Using the SciPy library we can find the minimum spanning tree
+
+Using the same API and folium we can create a html website with the map inside
 
 # TODOs
 
 - [X] research APIs to have distance for all points
 - [X] using this API to create a wheighted connected graph
-- [ ] find the shortest spanning tree
+- [X] find the shortest spanning tree
+- [ ] Complete the graph
 - [ ] solve using the Christofides algorithm
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from geopy.geocoders import Nominatim
 import numpy as np
+from numpy import savetxt
 import requests
+import time
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import minimum_spanning_tree
+import polyline
+import folium
 
 geolocator = Nominatim(user_agent="test")
 
@@ -17,7 +23,7 @@ class City:
         return str(self.lon)+","+str(self.lat)
 
 #popultate a list with name of the city and coordinate
-def populate (*string):
+def populate (string):
     list = []
     for str in string:
         temp = geolocator.geocode(str,timeout=None)
@@ -26,33 +32,83 @@ def populate (*string):
     
     return list
 
-def get_distance(string):
+def get_distance(dep_city,arr_city):
     #print(string)
+    string = dep_city.coordinate()+";"+arr_city.coordinate()
     url = 'http://router.project-osrm.org/route/v1/driving/'+string
     r = requests.get(url)
     res = r.json()
-    #print(res['routes'][0]['distance'])
+    #print(string)
+    if res["code"] == "NoRoute":
+        print("no route from " + str(dep_city)+" to "+ str(arr_city))
+        return -1
+
     distance = res['routes'][0]['distance']/1000
-    return round(distance,1)
+    return round(distance)
 
 #fill matrix with distances between city, only compute for lower trinagular matrix but we can assume the distance not changing much and cut in half the time soo...
 def fill_matrix(list):
     distance_matrix = np.zeros((len(list),len(list)))
     for x in range(len(list)):
         for y in range(x):
-            string = list[x].coordinate()+";"+list[y].coordinate()
-            distance_matrix[x][y] = get_distance(string)
+            #string = list[x].coordinate()+";"+list[y].coordinate()
+            distance_matrix[x][y] = get_distance(list[x],list[y])
     
     return np.tril(distance_matrix) + np.tril(distance_matrix, -1).T
 
+def print_geometry(dep_city,arr_city,m):
+    string = dep_city.coordinate()+";"+arr_city.coordinate()
+    url = 'http://router.project-osrm.org/route/v1/driving/'+string
+    r = requests.get(url)
+    res = r.json()
+    route =polyline.decode(res['routes'][0]['geometry'])
+
+
+    folium.PolyLine(
+        route,
+        weight=8,
+        color='blue',
+        opacity=0.6
+    ).add_to(m)
+
+    folium.Marker(
+        location=[dep_city.lat,dep_city.lon],
+        icon=folium.Icon(icon='play', color='green')
+    ).add_to(m)
+
+    folium.Marker(
+        location=[arr_city.lat,arr_city.lon],
+        icon=folium.Icon(icon='stop', color='red')
+    ).add_to(m)
+
+    return m
+
+
+
+
+def main():
+    place = ["Washington D.C.","New York","Boston","Buffalo, NY","Detroit","Chicago","San Francisco","San Jose","Los Angeles","Miami","Houston","New Orleans","Las Vegas","West Glacier, MT","Tusayan","Jackson Hole, WY","El Portal, CA"]
+    err = ["Washington D.C.","New York","Boston","Buffalo"]
+
+    start_time = time.perf_counter()
+    list = populate(place)
+    distance_matrix= fill_matrix(list)
+    end_time = time.perf_counter()
     
-list = populate("Bologna","Imola","Bari","Milano","Roma")
+    print("time ellapsed: "+ str(end_time-start_time))
+    
+    Tcsr = minimum_spanning_tree(distance_matrix)
+    minimum_tree = Tcsr.toarray()
+    
+    m = folium.Map(location=[ 40.072666376,-102.222165778],zoom_start=8)
+    
 
-distance_matrix= fill_matrix(list)
+    for x in range(len(list)):
+        for y in range(len(list)):
+            if minimum_tree[x][y]!=0:
+                m=print_geometry(list[x],list[y],m)
 
-#debug
-for city in list:
-    print(str(city))
+    m.save("map.html")
+    
 
-
-print(distance_matrix)
+main()
